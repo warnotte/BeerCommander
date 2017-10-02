1cc8a7dc591b3a03b204284fc1fdcc955e697d38
warnotte <renaud@warnotte.be>
2017-10-02 15:42:38 +0200

    Premier jet

---
+++ app/src/main/java/wax/application/org/beercommander/Commandes.java
@@ -0,0 +1,13 @@
+package wax.application.org.beercommander;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Commandes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_commandes);
+    }
+}
